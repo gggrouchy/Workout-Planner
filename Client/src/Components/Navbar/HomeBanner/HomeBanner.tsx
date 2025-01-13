846523a958abde6b732f2be16dacaa12e8ccf8e0
Ryan Fredericks <Kurt@stephanies-mbp.myfiosgateway.com>
2025-01-13 15:19:22 -0500

    Adding styling and editing values for input

--- Client/src/Components/Navbar/HomeBanner/HomeBanner.tsx
+++ Client/src/Components/Navbar/HomeBanner/HomeBanner.tsx
@@ -1,9 +1,46 @@
 import React from 'react'
+import './HomeBanner.css'
+
 
 const HomeBanner = () => {
+
+  const [data, setData] = React.useState<any>(null)
+
+  const getData = async () => {
+    let temp = [
+      {
+        "name": "Calories Intake",
+        "value": 2000,
+        "unit": 'Calories',
+        "goal": 2500,
+      },
+      {
+        "name": "Sleep",
+        "value": 8,
+        "unit": "hours",
+        "goal": 8
+      },
+      {
+        "name": "Workouts",
+        "value": 3,
+        "unit": "days",
+        "goal": 4
+      }
+    ]
+    setData(temp)
+  }
+  React.useEffect(() => {
+    getData()
+  }, [])
   return (
-    <div>
-      
+    <div className='meters'>
+      {
+        data?.length >0 && data.map((iten: any, index: number) =>{
+          <div className='card' key={index}>
+            
+            </div>
+        })
+      }
     </div>
   )
 }
--- Client/src/pages/index.tsx
+++ Client/src/pages/index.tsx
@@ -1,3 +1,4 @@
+"Use Client"
 import Head from "next/head";
 import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
